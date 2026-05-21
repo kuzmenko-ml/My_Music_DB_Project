75ba0b5de18c19b57d268f0272c7561754b3e553
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-05-21 13:53:20 +0300

    learning smth

---
+++ 04_learning_commands.sql
@@ -0,0 +1,54 @@
+USE My_Music_DB;
+GO
+
+SELECT * FROM Artists;
+
+SELECT ArtistNickname
+FROM Artists
+WHERE MusicGroup = 0;
+
+SELECT * FROM Users;
+
+SELECT UserID, UserName, SubscriptionType
+FROM Users
+WHERE SubscriptionType = 'Premium';
+
+SELECT * FROM Users 
+WHERE SubscriptionType = 'Premium';
+
+SELECT * FROM Tracks;
+
+SELECT Title, Genre
+FROM Tracks
+WHERE Genre = 'Pop';
+
+SELECT * FROM Tracks
+WHERE DurationSeconds > 200;
+
+SELECT * FROM Groups;
+
+SELECT GroupName 
+FROM Groups
+WHERE Followers > 10000 AND Country = 'Україна';
+
+SELECT * FROM Tracks
+WHERE Genre = 'Rock' OR Genre = 'Alternative';
+
+SELECT * FROM Tracks
+WHERE Genre IN ('Alternative', 'Rock');
+
+SELECT * FROM Artists
+WHERE ArtistNickname LIKE 'П%';
+
+SELECT Title AS [SongName]
+FROM Tracks;
+
+SELECT COUNT(*) FROM Users;
+
+SELECT MAX(DurationSeconds) AS [Maximum]
+FROM Tracks;
+SELECT MIN(DurationSeconds) AS [Minumum]
+FROM Tracks;
+
+SELECT MAX(DurationSeconds) AS [Maximum], MIN(DurationSeconds) AS [Minumum]
+FROM Tracks;
