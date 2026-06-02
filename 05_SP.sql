3a80df78e81e42c2b320c8778abc0d776395350b
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-06-02 21:27:56 +0300

    new SP TrackWithCheckingDS

--- 05_SP.sql
+++ 05_SP.sql
@@ -7,3 +7,21 @@ AS
 BEGIN
 SELECT * FROM Tracks WHERE Genre = @GenreName;
 END;
+
+CREATE PROCEDURE TrackWithCheckingDS
+	@Title NVARCHAR(150),
+	@Artist NVARCHAR(100),
+	@Genre NVARCHAR(50),
+	@DurationSeconds INT
+AS
+BEGIN
+	IF (@DurationSeconds > 0)
+		BEGIN
+			INSERT INTO Tracks (Title, Artist, Genre, DurationSeconds) VALUES
+			(@Title, @Artist, @Genre, @DurationSeconds);
+		END
+	ELSE 
+		BEGIN
+			PRINT 'Помилка: тривалість має бути більшою за 0'
+		END
+END;
