0c8a8b09adbecf5087d54738ebef429e469e138f
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-06-03 16:00:42 +0300

    learning SP

--- 05_SP.sql
+++ 05_SP.sql
@@ -25,3 +25,65 @@ BEGIN
 			PRINT 'Помилка: тривалість має бути більшою за 0'
 		END
 END;
+
+CREATE PROCEDURE SP_ListeningHistoryAddNew
+	@UserID INT,
+	@TrackID INT
+AS
+BEGIN
+	INSERT INTO ListeningHistory (UserID,TrackID) VALUES (@UserID,@TrackID )
+END;
+
+CREATE PROCEDURE SP_GetTracksByGenre
+	@TargetGenre NVARCHAR(50)
+AS
+BEGIN
+	SELECT * FROM Tracks
+	WHERE Genre = @TargetGenre;
+END;
+
+CREATE PROCEDURE SP_AddFollowersToGroup
+	@GroupName NVARCHAR(50),
+	@NewFollowers INT
+AS
+BEGIN
+	UPDATE Groups
+	SET Followers = Followers + @NewFollowers
+	WHERE GroupName = @GroupName;
+END;
+
+CREATE PROCEDURE SP_AddTrackWithArtistCheck
+	@Title NVARCHAR(150),
+	@Artist NVARCHAR(100),
+	@Genre NVARCHAR(50),
+	@DurationSeconds INT
+AS
+BEGIN
+	IF EXISTS (SELECT 1 FROM Artists WHERE ArtistNickname = @Artist)
+	BEGIN
+		INSERT INTO Tracks (Title, Artist, Genre, DurationSeconds) 
+		VALUES (@Title,@Artist, @Genre, @DurationSeconds)
+	END
+	ELSE
+		BEGIN
+			PRINT 'Помилка: такого артиста немає в базі даних!';
+		END
+END;
+
+CREATE PROCEDURE SP_AddTrackWithArtistAndDSCheck
+	@Title NVARCHAR(150),
+	@Artist NVARCHAR(100),
+	@Genre NVARCHAR(50),
+	@DurationSeconds INT
+AS
+BEGIN
+	IF (@DurationSeconds > 0) AND EXISTS (SELECT 1 FROM Artists WHERE ArtistNickname = @Artist)
+	BEGIN
+		INSERT INTO Tracks (Title, Artist, Genre, DurationSeconds) 
+		VALUES (@Title,@Artist, @Genre, @DurationSeconds)
+	END
+	ELSE
+	BEGIN
+		PRINT 'Помилка!';
+	END
+END;
