0b5b9128982e9c907bd22316c3615a3020dd4df9
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-06-03 16:17:17 +0300

    new TRIGGERS

--- 06_TR.sql
+++ 06_TR.sql
@@ -8,3 +8,25 @@ AS
 BEGIN
 	PRINT 'Новий трек успішно додано в систему!';
 END;
+
+CREATE TRIGGER TR_Users_AfterDelete
+ON Users
+AFTER DELETE
+AS
+BEGIN
+	DELETE FROM ListeningHistory
+	WHERE UserID IN (SELECT UserID FROM deleted);
+END;
+
+
+CREATE TRIGGER TR_Groups_CheckName
+ON Groups
+AFTER INSERT
+AS
+BEGIN
+	IF EXISTS (SELECT 1 FROM inserted WHERE GroupName = '')
+	BEGIN
+		PRINT 'Помилка: Назва групи не може бути порожньою!'
+		ROLLBACK TRANSACTION;
+	END
+END;
